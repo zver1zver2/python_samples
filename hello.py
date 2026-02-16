2855fa33abb9112abfe24d2032aa51181c7cbd1c
Zver Zverik <zver1zver2@gmail.com>
2026-02-16 20:42:04 +0300

    Добавил своё имя и ещё пару print-ов
    Added a new line to print GitHub nickname and enthusiasm.
    Yes. I did it.

--- hello.py
+++ hello.py
@@ -2,3 +2,7 @@
 print("Привет, GitHub!")
 print("Меня зовут", "zver1zver2")
 print("Сегодня", "я начал учить Git и GitHub")
+# Добавляем ещё одну строку
+name = "zver1zver2"
+print("Мой ник на GitHub:", name)
+print("GitHub — это круто!")
