c55fd3579c21eb3dffee983d6a9d30d7cd176098
Zver Zverik <zver1zver2@gmail.com>
2026-02-17 12:34:59 +0300

    Старая рабочая версия
    Вернулся к старой версии файла

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("Я всё сломал!")
+# Это мой первый файл на GitHub
+print("Привет, GitHub!")
+print("Меня зовут", "zver1zver2")
+print("Сегодня", "я начал учить Git и GitHub")
+# Добавляем ещё одну строку
+name = "zver1zver2"
+print("Мой ник на GitHub:", name)
+print("GitHub — это круто!")
