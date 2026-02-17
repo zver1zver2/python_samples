f71b7e276954f9f6c9225199f2ffacf2ecf1f4ac
Zver Zverik <zver1zver2@gmail.com>
2026-02-17 08:37:30 +0300

    Update hello.py
    Я сломал файл нарочно для теста.

--- hello.py
+++ hello.py
@@ -1,8 +1,1 @@
-# Это мой первый файл на GitHub
-print("Привет, GitHub!")
-print("Меня зовут", "zver1zver2")
-print("Сегодня", "я начал учить Git и GitHub")
-# Добавляем ещё одну строку
-name = "zver1zver2"
-print("Мой ник на GitHub:", name)
-print("GitHub — это круто!")
+print("Я всё сломал!")
