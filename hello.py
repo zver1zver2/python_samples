72ea820a32e5792125fbbc81948f0aa55d0363cd
Zver Zverik <zver1zver2@gmail.com>
2026-02-18 11:59:33 +0300

    Add header line and new print statement

--- hello.py
+++ hello.py
@@ -1,20 +1,2 @@
 # Это мой первый файл на GitHub
-print("Привет, GitHub!")
-print("Меня зовут", "zver1zver2")
-print("Сегодня", "я начал учить Git и GitHub")
-# Добавляем ещё одну строку
-name = "zver1zver2"
-print("Мой ник на GitHub:", name)
-print("GitHub — это круто!")
-# Эксперимент в ветке
-age = 25  # или твой возраст :)
-print(f"Мне {age} лет, и я учу GitHub!")
-print("Это изменение только в ветке, main пока не трогаем")
-
-# Это новый код в branch.002
-revenue = (100, 120, 200, 155, 301)
-for i in revenue:
-  print(i)
-
-# Just new code
-print("Hello world")
+print("=== Это мой первый файл на GitHub ===")
